b13f2f0018de5d9f4c73d91f70628c14cf1bf899
hironori shimada <shimada.hironori@nifty.co.jp>
2025-05-21 03:54:54 +0000

    test: fix handle empty raw query in rawQueryValues function

--- internal/handler/endpoint_test.go
+++ internal/handler/endpoint_test.go
@@ -1,33 +1,15 @@
 package handler_test
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 	"reflect"
-	"strings"
 	"testing"
+
+	"github.com/dev-shimada/gostubby/internal/handler"
 )
 
 func Test_rawQueryValues(t *testing.T) {
-	// テスト用の簡易rawQueryValues実装
-	customRawQueryValues := func(r http.Request) (url.Values, error) {
-		ret := url.Values{}
-		if r.URL.RawQuery == "" {
-			return ret, nil
-		}
-
-		parts := strings.Split(r.URL.RawQuery, "&")
-		for _, part := range parts {
-			kv := strings.Split(part, "=")
-			if len(kv) != 2 {
-				return nil, fmt.Errorf("invalid query parameter: %s", part)
-			}
-			ret.Add(kv[0], kv[1])
-		}
-		return ret, nil
-	}
-
 	type args struct {
 		r http.Request
 	}
@@ -81,7 +63,7 @@ func Test_rawQueryValues(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// 元の実装ではなく、テスト用の実装を使用
-			got, err := customRawQueryValues(tt.args.r)
+			got, err := handler.ExportedRawQueryValues(tt.args.r)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("rawQueryValues() error = %v, wantErr %v", err, tt.wantErr)
 				return
